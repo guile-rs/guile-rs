c6e67b17c41beab9fca7d9bb3b43e1f2501e4c5d
Clark Fischer <439978+clarkf@users.noreply.github.com>
2024-03-21 20:19:00 +0000

    Dependency and binding updates (#12)
    * Update bindgen
    
    Unable to compile with Guile 3.x due to a clang update, which caused
    bindgen to break:
    
    https://github.com/rust-lang/rust-bindgen/issues/2312
    
    This also updates the bindings to GNU Guile 3.0.9.
    
    * Update rust edition
    
    Use the most recent rust edition (2021) so that any future code is
    conformant.
    
    * Fix clippy lint issues
    
    Fix a trivial issue discovered by `cargo clippy'.

--- examples/shell.rs
+++ examples/shell.rs
@@ -2,8 +2,7 @@ extern crate guile;
 
 fn main() {
     guile::init(|vm| {
-        let mut args = Vec::new();
-        args.push("Test".to_string());
+        let args = vec!["Test".to_string()];
         vm.shell(args);
     });
 }
